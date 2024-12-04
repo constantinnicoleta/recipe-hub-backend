086e72a9847d7e65b181ebe79521f9e62a196159
Nicoleta Constantin <constantinnicoleta@hotmail.com>
2024-12-04 01:09:32 +0000

    Set up token-based authentication with dj-rest-auth and rest_framework.authtoken

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
     'recipes',
     'cloudinary',
     'cloudinary_storage',
