91adb5f6cf40cf3950b488a7cabbdfa1237c15f2
Nicoleta <constantinnicoleta@hotmail.com>
2025-01-31 23:55:54 +0000

    update files

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('api/', include('recipes.urls')),
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
-     path('auth/token/',
+    path('auth/token/',
          TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/',
          TokenRefreshView.as_view(), name='token_refresh'),
